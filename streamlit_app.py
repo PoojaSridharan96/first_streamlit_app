a3645132f11ad0cd462bef0a14a744201321d583
PoojaSridharan96 <134591630+PoojaSridharan96@users.noreply.github.com>
2023-05-25 14:02:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('MY PARENTS NEW HEALTHY DINER')
+streamlit.header('BREAKFAST MEANU')
+streamlit.text('American Breakfast')
+streamlit.text('Hard boiled free range egg')
+streamlit.text('Kale,Spinach & Rocket Smoothie')
